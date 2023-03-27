9ad52c37c568b2dc927c8667a3b70c166ffbfc7b
JohnDigger <541554971@qq.com>
2023-03-27 17:23:06 +0800

    大数据项目修改

--- api/src/main/java/com/datamanage/datamanage/DatamanageApplication.java
+++ api/src/main/java/com/datamanage/datamanage/DatamanageApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DatamanageApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DatamanageApplication.class, args);
     }
