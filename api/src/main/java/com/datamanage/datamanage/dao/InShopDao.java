c2a397d4e8a4d6146ca2fc4d0426d0e508099caa
JohnDigger <541554971@qq.com>
2023-03-29 02:52:06 +0800

    更新

--- api/src/main/java/com/datamanage/datamanage/dao/InShopDao.java
+++ api/src/main/java/com/datamanage/datamanage/dao/InShopDao.java
@@ -2,6 +2,7 @@ package com.datamanage.datamanage.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.datamanage.datamanage.entity.InShopEntity;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -18,4 +19,6 @@ import java.util.List;
 public interface InShopDao extends BaseMapper<InShopEntity> {
 	@Select("SELECT * FROM in_shop WHERE in_shop.data_address = #{address} ORDER BY in_shop.sale_money")
     List<InShopEntity> getAll(String address);
+	@Insert("INSERT INTO in_shop (index, name,belongTo,dataAddress,time) VALUES (#{index}, #{name},#{belongTo},#{dataAddress},#{time}))")
+    int insert(InShopEntity inShopEntity);
 }
