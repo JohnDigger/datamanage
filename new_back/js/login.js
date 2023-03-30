403c1dd4599be285f9d0f0464381cb32efad694e
jianglaihong1 <2060736540@qq.com>
2023-03-30 18:42:20 +0800

    增加登录提示以及登录页面样式

--- new_back/js/login.js
+++ new_back/js/login.js
@@ -1,9 +1,10 @@
+
+
 function submit(){
     var requestData = {
         username: $("#username").val(),
         password: $("#password").val()
     };
-    console.log(requestData)
     $.ajax({
         type: 'POST',
         contentType: "application/json",
@@ -11,12 +12,20 @@ function submit(){
         data: JSON.stringify(requestData),
         success: function (response){
             window.localStorage.setItem("token",response.token);
+            let loginErrorDiv = window.document.getElementById("loginErrorId");
+            loginErrorDiv.style.display = "hidden";
             window.location.href="areaSelect.html";
+
         },
         error: function(xhr, status, error) {
+            let loginErrorDiv = window.document.getElementById("loginErrorId");
+            console.log(1111111);
+            loginErrorDiv.style.visibility = "visible";
             // 请求失败的回调函数
             console.log(error);
         }
     });
 
 }
+
+
