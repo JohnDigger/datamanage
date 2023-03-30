bbc999f64978a836b36f002d6ed7c77848f46ebc
jianglaihong1 <2060736540@qq.com>
2023-03-30 16:49:50 +0800

    完成登录跳转

--- new_back/js/addArea.js
+++ new_back/js/addArea.js
@@ -4,5 +4,4 @@ let areaNameInput = window.document.getElementById("addressName");
 let areaName = areaNameInput;
 checkAreaButton.addEventListener("click", function (){
     window.location.href="dateEdit.html?areaName="+areaName.valueOf().value;
-
 })
--- new_back/js/areaSelectJs.js
+++ new_back/js/areaSelectJs.js
@@ -1,6 +1,11 @@
 
 let checkAreaButton = window.document.getElementById("checkAreaButton");
+let addAreaButton = window.document.getElementById("addAreaButton");
 let areaName = checkAreaButton.innerText;
 checkAreaButton.addEventListener("click", function (){
     window.location.href="dateEdit.html?areaName="+areaName;
+})
+
+addAreaButton.addEventListener("click", function (){
+    window.location.href="addArea.html";
 })
