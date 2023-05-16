92e1785ec372ec64f7255237f4c9006cb8c5b4e5
JohnDigger <541554971@qq.com>
2023-05-16 15:58:21 +0800

    大数据项目修改

--- api/src/main/java/com/datamanage/datamanage/dao/InTopThirtyDao.java
+++ api/src/main/java/com/datamanage/datamanage/dao/InTopThirtyDao.java
@@ -18,7 +18,6 @@ import java.util.List;
 public interface InTopThirtyDao extends BaseMapper<InTopThirtyEntity> {
 	@Select("select * FROM in_top_thirty WHERE in_top_thirty.data_address = #{address} and in_top_thirty.data_date = #{date_time}  ORDER BY in_top_thirty.sale_money DESC LIMIT 30")
     List<InTopThirtyEntity> getTopThirty(String address,String date_time);
-
 	@Select("SELECT MAX(CAST(in_top_thirty.sale_money AS UNSIGNED)) FROM in_top_thirty WHERE in_top_thirty.data_address = #{address} AND in_top_thirty.data_date = #{data_date}")
     Integer getTop(String address,String data_date);
 
